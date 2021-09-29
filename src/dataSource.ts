f3222cf5c670cd2e3ff5db4734834c785b00acc9
Ben Nash <bennash@gmail.com>
2021-09-29 16:51:35 -0700

    my key

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,7 +1,7 @@
 import { RequestOptions, RESTDataSource } from 'apollo-datasource-rest'
 import camelCaseKeys from 'camelcase-keys'
 
-const ACCESS_KEY = '9d8910ead74d9a3e0931ca9881f4b9cd'
+const ACCESS_KEY = 'df71b4d7c91570a0792f88b2301268e2'
 const API_URL = 'https://api.openweathermap.org/data/2.5/'
 
 export class WeatherAPI extends RESTDataSource {
